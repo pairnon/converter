2eee62ee4fe8ec5121aafdc7ae8d9cb35720d199
pairnon <janpairnon@gmail.com>
2024-02-22 10:24:56 -0800

    Add files

---
+++ public/converter.js
@@ -0,0 +1,67 @@
+
+const btcURL = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
+const ethURL = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD';
+
+document.getElementById('USDtoBTC').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const usdInput = document.getElementById('usd').value;
+    
+    fetch(btcURL)
+        .then(response => response.json())
+        .then(data => {
+            const btcAmount = usdInput / data.USD;
+            document.getElementById('btcValue').innerText = `BTC: ${btcAmount.toFixed(8)}`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            document.getElementById('btcValue').innerText = 'Error fetching data.';
+        });
+});
+
+document.getElementById('BTCtoUSD').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const btcInput = document.getElementById('btc').value;
+    
+    fetch(btcURL)
+        .then(response => response.json())
+        .then(data => {
+            const usdAmount = btcInput * data.USD;
+            document.getElementById('usdValue').innerText = `USD: ${usdAmount.toFixed(8)}`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            document.getElementById('usdValue').innerText = 'Error fetching data.';
+        });
+});
+
+document.getElementById('USDtoETH').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const usdInput = document.getElementById('eusd').value;
+    
+    fetch(ethURL)
+        .then(response => response.json())
+        .then(data => {
+            const ethAmount = usdInput / data.USD;
+            document.getElementById('ethValue').innerText = `ETH: ${ethAmount.toFixed(8)}`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            document.getElementById('ethValue').innerText = 'Error fetching data.';
+        });
+});
+
+document.getElementById('ETHtoUSD').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const ethInput = document.getElementById('eth').value;
+    
+    fetch(ethURL)
+        .then(response => response.json())
+        .then(data => {
+            const usdAmount = ethInput * data.USD;
+            document.getElementById('eusdValue').innerText = `USD: ${usdAmount.toFixed(8)}`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            document.getElementById('eusdValue').innerText = 'Error fetching data.';
+        });
+});
